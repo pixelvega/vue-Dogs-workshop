235b3e00f8d203d2112ec14d75afad6444bd2e1a
pixelvega <43549174+pixelvega@users.noreply.github.com>
2018-12-04 20:46:49 +0100

    Initial commit

---
+++ src/components/dog.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card class="dog-card">
+    <v-img height="150px" :src="dog"></v-img>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="removeFromFavorites(index);">
+        <v-icon>delete</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    dog: {
+      type: String
+    }
+  }
+};
+</script>
